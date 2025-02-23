6b5b909a883656cb3655b141b3718e6be77b2ee7
Tom Hodemon <tomhodemon3@gmail.com>
2025-02-23 10:05:40 +0100

    wip

--- data.py
+++ data.py
@@ -1,43 +1,54 @@
-from torch.utils.data import Dataset
 import json
-import pathlib
+import os
+from pathlib import Path
+from typing import List
+from pydantic import BaseModel
+from torch.utils.data import Dataset
+
+class Message(BaseModel):
+    role: str
+    content: str
+
+class Conversation(BaseModel):
+    id: str
+    image: str
+    conversations: List[Message]
 
 class LLaVAInstruct(Dataset):
-    """
-    A dataset for the LLaVA Instruct dataset. It returns a dict with the following keys:
-    - "id": The id of the conversation.
-    - "image": The image file name.
-    - "conversation": A list of dicts with the following keys:
-        - "role": The role of the speaker (always "human" for this dataset)
-        - "content": The content of the message.
-    """
     def __init__(self, 
-                 data_path: str,
-                 image_dir: str,
-                 ) -> None:
-    
+                 data_path: Path, 
+                 image_dir: Path
+                ) -> None:
+        
         self.data = json.load(open(data_path, "r"))
-        self.image_dir = pathlib.Path(image_dir)
+        self.image_dir = image_dir
 
         print(f"Loaded {len(self.data)} conversation samples from {data_path}")
         print(f"Image directory: {self.image_dir}")
-        
-    def __len__(self):
+
+    def __len__(self) -> int:
         return len(self.data)
-    
-    def __getitem__(self, idx):
+
+    def __getitem__(self, idx: int) -> Conversation:
         sample = self.data[idx]
-        conversation = sample["conversations"]
-        image_path = self.image_dir / sample["image"]
-        return {
-            "id": sample["id"],
-            "image": image_path,
-            "conversation": conversation
-        }
-    
+        image_path = os.path.join(self.image_dir, sample['image'])
+        sample['image'] = image_path
+        
+        for conv in sample['conversations']:
+            conv['role'] = conv.pop('from')
+            conv['content'] = conv.pop('value')
+        
+        return Conversation(**sample)
+
 
 if __name__ == "__main__":
-    # For testing
-    dataset = LLaVAInstruct(data_path= "./datasets/complex_reasoning_77k.json")
-    print(len(dataset))
-    print(dataset[1])
+    # Example usage
+
+    datasets_dir = Path('/Users/thod/workspace/datasets')
+    dataset = LLaVAInstruct(
+        data_path=datasets_dir / 'llava-instruct/complex_reasoning_77k.json', 
+        image_dir=datasets_dir / 'coco/train2017'
+    )
+
+    example = dataset[0]
+    print(example)
